f918924b14df7a6270bed5b7c101220f9a415030
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 17:12:40 -0600

    Update item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
-# FactoryBot.define do
-#   factory :items do
-#     name (Faker::Coffee.blend_name)
-#     description (Faker::Coffee.notes )
-#     unit_price (Faker::Number.decimal(l_digits: 2))
-#     merchant_id (Faker::Number.number(digits: 10))
-#
-#     after :create do |item|
-#       invoice = create(:invoice)
-#       create :invoice_items, item: item, invoice: invoice
-#     end
-#   end
-# end
+FactoryBot.define do
+  factory :item do
+    name (Faker::Coffee.blend_name)
+    description (Faker::Coffee.notes )
+    unit_price (Faker::Number.decimal(l_digits: 2))
+    merchant_id (Faker::Number.number(digits: 10))
+
+    after :create do |item|
+      invoice = create(:invoice)
+      create :invoice_items, item: item, invoice: invoice
+    end
+  end
+end
