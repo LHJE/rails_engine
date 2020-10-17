6df3a308c62b6bc15f006c20975941e7adcb5668
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 11:13:20 -0600

    Update load merchant model method to create merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,7 +6,7 @@ class Merchant < ApplicationRecord
   def self.load_merchants(file_path)
     merchants = []
     CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
-      merchants << Merchant.new(data.to_h)
+      merchants << Merchant.create!(name: data.to_h[:name])
     end
     merchants
   end
