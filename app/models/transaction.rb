49d59928b9c54949ea57f6d790630a65a82b7912
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 10:59:17 -0600

    Add method to transaction to load transaction csv

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -9,4 +9,12 @@ class Transaction < ApplicationRecord
   validates :invoice_id,
            :credit_card_number,
            :result, presence: true
+
+  def self.load_transactions(file_path)
+    transactions = []
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
+      transactions << Transaction.new(data.to_h)
+    end
+    transactions
+  end
 end
