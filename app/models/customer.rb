fa6969f51e13596674e06cb8af62a1f66a8236f4
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 13:29:34 -0600

    Update customer model with model method for loading customer csv

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
 
   validates :first_name,
            :last_name, presence: true
+
+
+  def self.load_customers(file_path)
+    customers = []
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
+      customers << Customer.create!(first_name: data.to_h[:first_name], last_name: data.to_h[:last_name])
+    end
+    customers
+  end
 end
