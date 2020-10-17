532c8cd562843d852482103922f58dd0eec2b09f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 17:12:53 -0600

    Update invoice item factory

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -1,8 +1,8 @@
-# FactoryBot.define do
-#   factory :invoice_items do
-#     association :invoices
-#     association :items
-#     quantity (Faker::Number.decimal_part(digits: 2))
-#     unit_price (Faker::Number.decimal(l_digits: 2))
-#   end
-# end
+FactoryBot.define do
+  factory :invoice_item do
+    association :invoices
+    association :items
+    quantity (Faker::Number.decimal_part(digits: 2))
+    unit_price (Faker::Number.decimal(l_digits: 2))
+  end
+end
