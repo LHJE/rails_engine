e9ee4b77bf1d8c542cdc824c3ac609a5973abb54
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-16 22:26:18 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_041824) do
+ActiveRecord::Schema.define(version: 2020_10_17_042443) do
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
