ea14063982444da95457ff0511493b91956da649
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 13:29:05 -0600

    Update item model with model method for loading item csv

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,12 @@ class Item < ApplicationRecord
            :description,
            :unit_price,
            :merchant_id, presence: true
+
+  def self.load_items(file_path)
+    items = []
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
+      items << Item.create!(name: data.to_h[:name], description: data.to_h[:description], unit_price: data.to_h[:unit_price], merchant_id: data.to_h[:merchant_id])
+    end
+    items
+  end
 end
