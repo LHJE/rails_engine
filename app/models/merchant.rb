6086b3b74930ca7e271a165bbbb433da38297bcb
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 13:29:00 -0600

    Update merchant model with model method for loading merchant csv

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   validates :name, presence: true
+
+  def self.load_merchants(file_path)
+    merchants = []
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
+      merchants << Merchant.create!(name: data.to_h[:name])
+    end
+    merchants
+  end
 end
