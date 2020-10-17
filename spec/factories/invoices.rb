2095cc0bb27878eecbb88d669b11cdcfee6ca708
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 17:12:45 -0600

    Update invoice factory

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,7 +1,7 @@
-# FactoryBot.define do
-#   factory :invoices do
-#     customer_id (Faker::IDNumber.brazilian_id)
-#     merchant_id (Faker::Number.number(digits: 10))
-#     status (Faker::Boolean.boolean)
-#   end
-# end
+FactoryBot.define do
+  factory :invoice do
+    customer_id (Faker::IDNumber.brazilian_id)
+    merchant_id (Faker::Number.number(digits: 10))
+    status (Faker::Boolean.boolean)
+  end
+end
