cfcf9aa47bfa53645916ad8b14134e283d9f3fb8
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-16 21:06:58 -0600

    Add transactions factory for potential use of FactoryBot later

---
+++ spec/factories/transactions.rb
@@ -0,0 +1,8 @@
+# FactoryBot.define do
+#   factory :transactions do
+#     invoice_id (Faker::Crypto.md5)
+#     credit_card_number (Faker::Number.number(digits: 16))
+#     credit_card_expiration_date (12/30)
+#     result (Faker::Boolean.boolean)
+#   end
+# end
