76b223fad4fa77e0dccd977e93613caa0698dcca
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 16:53:44 -0600

    Add test to customer request spec for specific customer

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -23,4 +23,23 @@ describe "Customers API" do
       expect(customer[:last_name]).to be_a(String)
     end
   end
+
+  it "sends a list of customers" do
+    id = create(:customer).id
+
+    get "/api/v1/customers/#{id}"
+
+    expect(response).to be_successful
+
+    customer = JSON.parse(response.body, symbolize_names: true)
+
+    expect(customer).to have_key(:id)
+    expect(customer[:id]).to eq(id)
+
+    expect(customer).to have_key(:first_name)
+    expect(customer[:first_name]).to be_a(String)
+
+    expect(customer).to have_key(:last_name)
+    expect(customer[:last_name]).to be_a(String)
+  end
 end
