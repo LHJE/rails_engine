3a798aed58c20bf469a1a7c0eea9d586a71857f3
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 17:12:23 -0600

    Update merchant factory

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,5 @@
-# FactoryBot.define do
-#   factory :merchants do
-#     name (Faker::IndustrySegments.sub_sector)
-#   end
-# end
+FactoryBot.define do
+  factory :merchant do
+    name (Faker::IndustrySegments.sub_sector)
+  end
+end
