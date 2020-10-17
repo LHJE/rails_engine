e760db248e927ba57073df1e05fb3f94585f7d12
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 13:27:09 -0600

    Add test to customer spec for method to load customer csv

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
-describe Customer, type: :model do
+RSpec.describe Customer, type: :model do
   describe "validations" do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
+  end
+
+  describe 'Class Methods' do
+    it "can load customers" do
+      Customer.load_customers('spec/fixtures/files/customers_fixtures.csv')
+      expect("#{Customer.where(id: 1).class}").to eq("Customer::ActiveRecord_Relation")
+    end
   end
 end
