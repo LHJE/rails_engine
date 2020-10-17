82368a8c93081bc4355e79a71031a90786115347
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 13:26:59 -0600

    Add test to transaction spec for method to load transaction csv

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
-describe Transaction, type: :model do
+RSpec.describe Transaction, type: :model do
   describe "validations" do
     it { should validate_presence_of :invoice_id }
     it { should validate_presence_of :credit_card_number }
-    # it { should validate_presence_of :credit_card_expiration_date }
     it { should validate_presence_of :result }
+  end
+
+  describe 'Class Methods' do
+    it "can load transactions" do
+      Transaction.load_transactions('spec/fixtures/files/transactions_fixtures.csv')
+
+      expect("#{Transaction.where(id: 1).class}").to eq("Transaction::ActiveRecord_Relation")
+    end
   end
 end
