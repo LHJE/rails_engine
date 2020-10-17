7ea665b4c5e73adf1c68a8cd1bebb59eeada7e59
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 16:52:53 -0600

    Comment out potentially unneeded method in api poros

--- app/POROS/api.rb
+++ app/POROS/api.rb
@@ -1,12 +1,12 @@
 class Api
 
-  def load_api(path, customer_file, merchant_file, item_file, invoice_file, invoice_item_file, transaction_file)
-    customers = Customer.load_customers('#{path}/#{customer_file}')
-    merchants = Merchant.load_merchants('#{path}/#{merchant_file}')
-    items = Item.load_items('#{path}/#{item_file}')
-    invoices = Invoice.load_invoices('#{path}/#{invoice_file}')
-    invoice_items = InvoiceItem.load_invoice_items('#{path}/#{invoice_item_file}')
-    transactions = Transaction.load_transactions('#{path}/#{transaction_file}')
-    Api.create!(customers: customers, merchants: merchants, items: items, invoices: invoices, invoice_items: invoice_items, transactions: transactions)
-  end
+  # def load_api(path, customer_file, merchant_file, item_file, invoice_file, invoice_item_file, transaction_file)
+  #   customers = Customer.load_customers('#{path}/#{customer_file}')
+  #   merchants = Merchant.load_merchants('#{path}/#{merchant_file}')
+  #   items = Item.load_items('#{path}/#{item_file}')
+  #   invoices = Invoice.load_invoices('#{path}/#{invoice_file}')
+  #   invoice_items = InvoiceItem.load_invoice_items('#{path}/#{invoice_item_file}')
+  #   transactions = Transaction.load_transactions('#{path}/#{transaction_file}')
+  #   Api.create!(customers: customers, merchants: merchants, items: items, invoices: invoices, invoice_items: invoice_items, transactions: transactions)
+  # end
 end
