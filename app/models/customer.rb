79ccb8a1bd0bb333e634da5f34555b884add3b93
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 11:12:52 -0600

    Update load customers model method to create customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,7 +8,7 @@ class Customer < ApplicationRecord
   def self.load_customers(file_path)
     customers = []
     CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
-      customers << Customer.new(data.to_h)
+      customers << Customer.create!(first_name: data.to_h[:first_name], last_name: data.to_h[:last_name])
     end
     customers
   end
