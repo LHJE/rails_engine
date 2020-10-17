ad8552ff4eaefbe59de629963955570f330e4856
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 10:40:24 -0600

    Comment out credit card experiation date from transaction model ... d/t csv's not having them

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,12 @@
 class Transaction < ApplicationRecord
   has_many :invoices
 
+  # validates :invoice_id,
+  #          :credit_card_number,
+  #          :credit_card_expiration_date,
+  #          :result, presence: true
+
   validates :invoice_id,
            :credit_card_number,
-           :credit_card_expiration_date,
            :result, presence: true
 end
