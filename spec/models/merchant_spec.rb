9b02f1f8f4117e4f10786426bfcd232a43a5b270
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 13:26:40 -0600

    Add test to merchant spec for method to load merchant csv

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
-describe Merchant, type: :model do
+RSpec.describe Merchant, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
+  end
+
+  describe 'Class Methods' do
+    it "can load merchants" do
+      Merchant.load_merchants('spec/fixtures/files/merchants_fixtures.csv')
+
+      expect("#{Merchant.where(id: 1).class}").to eq("Merchant::ActiveRecord_Relation")
+    end
   end
 end
