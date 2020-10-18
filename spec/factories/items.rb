061720a5b49d6e6b2cf93367d64ab970b83bba15
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 20:19:35 -0600

    Delete after :create from itmes factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,10 +4,5 @@ FactoryBot.define do
     description { Faker::Coffee.notes }
     unit_price { Faker::Number.decimal(l_digits: 2) }
     merchant_id { Faker::Number.number(digits: 10) }
-
-    after :create do |item|
-      invoice = create(:invoice)
-      create :invoice_items, item: item, invoice: invoice
-    end
   end
 end
