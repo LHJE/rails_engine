0ab69e2f323702c7683b7bfc6d44ffc202420c85
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 18:00:28 -0600

    Update items factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :item do
-    name (Faker::Coffee.blend_name)
-    description (Faker::Coffee.notes )
-    unit_price (Faker::Number.decimal(l_digits: 2))
-    merchant_id (Faker::Number.number(digits: 10))
+    name { Faker::Coffee.blend_name }
+    description { Faker::Coffee.notes }
+    unit_price { Faker::Number.decimal(l_digits: 2) }
+    merchant_id { Faker::Number.number(digits: 10) }
 
     after :create do |item|
       invoice = create(:invoice)
