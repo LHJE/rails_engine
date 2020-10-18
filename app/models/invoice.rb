c88c2f5dcf3b34e2b89b08895fe59c978792dbcd
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 20:33:05 -0600

    Update invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,6 @@
 class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
-  has_many :transactions
+  has_many :transactions, through: :transaction
 
   belongs_to :customer, optional: true
   belongs_to :merchant, optional: true
