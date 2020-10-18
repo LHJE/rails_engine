cefc65d760a39ee545d36e70253fce15a7d3abf3
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 20:20:21 -0600

    Update invoice items model's belong to with 'optional: true'

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,6 @@
 class InvoiceItem < ApplicationRecord
-  belongs_to :item
-  belongs_to :invoice
+  belongs_to :item, optional: true
+  belongs_to :invoice, optional: true
 
   validates :item_id,
             :invoice_id,
