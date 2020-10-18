3bc4331e945f3327b56e66c4dfc0c718d5c532e9
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 18:52:35 -0600

    Update models commenting out potentially unecessary methods

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,11 +7,11 @@ class InvoiceItem < ApplicationRecord
            :quantity,
            :unit_price, presence: true
 
-  def self.load_invoice_items(file_path)
-    invoice_items = []
-    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
-      invoice_items << InvoiceItem.create!(item_id: data.to_h[:item_id], invoice_id: data.to_h[:invoice_id], quantity: data.to_h[:quantity], unit_price: data.to_h[:unit_price])
-    end
-    invoice_items
-  end
+  # def self.load_invoice_items(file_path)
+  #   invoice_items = []
+  #   CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
+  #     invoice_items << InvoiceItem.create!(item_id: data.to_h[:item_id], invoice_id: data.to_h[:invoice_id], quantity: data.to_h[:quantity], unit_price: data.to_h[:unit_price])
+  #   end
+  #   invoice_items
+  # end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,11 +3,11 @@ class Merchant < ApplicationRecord
 
   validates :name, presence: true
 
-  def self.load_merchants(file_path)
-    merchants = []
-    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
-      merchants << Merchant.create!(name: data.to_h[:name])
-    end
-    merchants
-  end
+  # def self.load_merchants(file_path)
+  #   merchants = []
+  #   CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
+  #     merchants << Merchant.create!(name: data.to_h[:name])
+  #   end
+  #   merchants
+  # end
 end
