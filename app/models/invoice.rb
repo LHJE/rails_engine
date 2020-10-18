8bdd14fa73f7bbe2cb02896b67e9bcf5808e808f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 20:20:13 -0600

    Update invoice model's belong to with 'optional: true'

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,8 +2,8 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
   has_many :transactions
 
-  belongs_to :customer
-  belongs_to :merchant
+  belongs_to :customer, optional: true
+  belongs_to :merchant, optional: true
 
   validates :customer_id,
             :merchant_id,
