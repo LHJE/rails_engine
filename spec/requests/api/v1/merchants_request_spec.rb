18e140d1060d08ad2e9e876c3e290ace51dd525f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-17 18:11:24 -0600

    Update merchants with test for show

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -21,4 +21,20 @@ describe "Merchants API" do
     end
   end
 
+  it "sends a list of merchants" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
+
 end
