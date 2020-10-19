d2d5e700c883acafd02654e03e94f13f868cfc4f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-19 17:05:02 -0600

    Update test to get individual customer call with new serializer method

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -31,15 +31,15 @@ describe "Customers API" do
     expect(response).to be_successful
 
     customer = JSON.parse(response.body, symbolize_names: true)
-    # require "pry"; binding.pry
-    expect(customer).to have_key(:id)
-    expect(customer[:id]).to eq(id)
 
-    expect(customer).to have_key(:first_name)
-    expect(customer[:first_name]).to be_a(String)
+    expect(customer[:data]).to have_key(:id)
+    expect(customer[:data][:id]).to eq("#{id}")
 
-    expect(customer).to have_key(:last_name)
-    expect(customer[:last_name]).to be_a(String)
+    expect(customer[:data]).to have_key(:first_name)
+    expect(customer[:data][:first_name]).to be_a(String)
+
+    expect(customer[:data]).to have_key(:last_name)
+    expect(customer[:data][:last_name]).to be_a(String)
   end
 
   it "can create a new customer" do
