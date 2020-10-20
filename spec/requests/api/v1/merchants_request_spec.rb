9616d927ce61aa306629d6d4b15e8a21c9c1e1fd
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-19 21:45:10 -0600

    Update merchants spec to get items specific to that merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -84,4 +84,29 @@ describe "Merchants API" do
     expect(response).to be_success
     expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  it "can get the items specific to this merchant" do
+    create_list(:item, 3)
+    get '/api/v1/items'
+
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}/items"
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    items[:data].each do |item|
+      expect(item[:attributes]).to have_key(:id)
+      expect(item[:attributes][:id]).to be_an(Integer)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+  end
 end
