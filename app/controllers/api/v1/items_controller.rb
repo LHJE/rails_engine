7f8a38cc365f11bcf8529059b3491615ba2f8071
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-19 20:53:17 -0600

    Add serializer logic to items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,23 +2,23 @@ module Api
   module V1
     class ItemsController < ApplicationController
       def index
-        render json: Item.all
+        render json: ItemSerializer.new(Item.all)
       end
 
       def show
-        render json: Item.find(params[:id])
+        render json: ItemSerializer.new(Item.find(params[:id]))
       end
 
       def create
-        render json: Item.create(item_params)
+        render json: ItemSerializer.new(Item.create(item_params))
       end
 
       def update
-        render json: Item.update(params[:id], item_params)
+        render json: ItemSerializer.new(Item.update(params[:id], item_params))
       end
 
       def destroy
-        render json: Item.delete(params[:id])
+        render json: ItemSerializer.new(Item.delete(params[:id]))
       end
 
       private
