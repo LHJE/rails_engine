de1ef1d9c5c1939c1d852f7eac9fe8a8b7da732f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-20 13:55:31 -0600

    Update relatinoship logic in invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,7 @@
 class Invoice < ApplicationRecord
+  has_many :invoice_items
   has_many :items, through: :invoice_items
-  has_many :transactions, through: :transaction
+  has_many :transactions, through: :invoice_items
 
   belongs_to :customer, optional: true
   belongs_to :merchant, optional: true
