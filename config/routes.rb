5aa95971b243f525e2a532399676302d8607e7b2
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-20 21:30:33 -0600

    Update routes for all models

--- config/routes.rb
+++ config/routes.rb
@@ -5,28 +5,34 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :customers do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
       resources :customers
       namespace :invoice_items do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
       resources :invoice_items
       namespace :invoices do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
       resources :invoices
       namespace :items do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
       resources :items
       get "items/:id/merchants", to: 'items#merchant'
       namespace :merchants do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
       resources :merchants
       get "merchants/:id/items", to: 'merchants#items'
       namespace :transactions do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
       resources :transactions
     end
