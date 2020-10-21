6c6b58ba0fcc751dbe58f51061466d7d772cd520
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-20 18:43:34 -0600

    Update transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,11 +2,6 @@ class Transaction < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
-  # validates :invoice_id,
-  #          :credit_card_number,
-  #          :credit_card_expiration_date,
-  #          :result, presence: true
-
   validates :invoice_id,
             :credit_card_number,
             :result, presence: true
