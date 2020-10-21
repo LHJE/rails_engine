76bf1b49878da5d50c9fc49720cebf917eafae1c
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-20 19:25:03 -0600

    Update transaction search controller with search logic

--- app/controllers/api/v1/transactions/search_controller.rb
+++ app/controllers/api/v1/transactions/search_controller.rb
@@ -3,7 +3,15 @@ module Api
     module Transactions
       class SearchController < ApplicationController
         def show
-
+          if params.keys.include?("id")
+            render json: TransactionSerializer.new(Transaction.find(params[:id]))
+          elsif params.keys.include?("invoice_id")
+            render json: TransactionSerializer.new(Transaction.where("invoice_id like ?", "%#{params[:invoice_id]}%"))
+          elsif params.keys.include?("credit_card_number")
+            render json: TransactionSerializer.new(Transaction.where("credit_card_number like ?", "%#{params[:credit_card_number]}%"))
+          else
+            render json: TransactionSerializer.new(Transaction.where("result like ?", "%#{params[:result]}%"))
+          end
         end
       end
     end
