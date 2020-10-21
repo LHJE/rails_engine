e5bdf0e18660e6197817ceb59aa6d2c001965dfc
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-20 21:07:21 -0600

    Re-add load method to merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,15 @@
+require 'csv'
+
 class Merchant < ApplicationRecord
   has_many :invoices, through: :invoice
 
   validates :name, presence: true
+
+  def self.load_merchants(file_path)
+    merchants = []
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
+      merchants << Merchant.create!(name: data.to_h[:name])
+    end
+    merchants
+  end
 end
