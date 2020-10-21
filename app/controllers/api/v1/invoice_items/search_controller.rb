b7609556f0a48dc53e034a991e82791e7bab37e9
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-20 18:43:20 -0600

    Update invoice search controller with show logic

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -3,7 +3,17 @@ module Api
     module InvoiceItems
       class SearchController < ApplicationController
         def show
-
+          if params.keys.include?("id")
+            render json: InvoiceItemSerializer.new(InvoiceItem.find(params[:id]))
+          elsif params.keys.include?("item_id")
+            render json: InvoiceItemSerializer.new(InvoiceItem.where("item_id like ?", "%#{params[:item_id]}%"))
+          elsif params.keys.include?("invoice_id")
+            render json: InvoiceItemSerializer.new(InvoiceItem.where("invoice_id like ?", "%#{params[:invoice_id]}%"))
+          elsif params.keys.include?("quantity")
+            render json: InvoiceItemSerializer.new(InvoiceItem.where("quantity like ?", "%#{params[:quantity]}%"))
+          else
+            render json: InvoiceItemSerializer.new(InvoiceItem.where("unit_price like ?", "%#{params[:unit_price]}%"))
+          end
         end
       end
     end
