6f930d91bf442925272eae35e0fc035f7a431928
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-21 16:57:52 -0600

    Commenting out unnecessary controllers

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -1,25 +1,25 @@
-module Api
-  module V1
-    module Customers
-      class SearchController < ApplicationController
-        def show
-          if params.keys.include?("id")
-            render json: CustomerSerializer.new(Customer.find(params[:id]))
-          elsif params.keys.include?("first_name")
-            render json: CustomerSerializer.new(Customer.where("first_name like ?", "%#{params[:first_name]}%"))
-          else
-            render json: CustomerSerializer.new(Customer.where("last_name like ?", "%#{params[:last_name]}%"))
-          end
-        end
-
-        def index
-          if params.keys.include?("first_name")
-            render json: CustomerSerializer.new(Customer.where("first_name like ?", "%#{params[:first_name]}%"))
-          else
-            render json: CustomerSerializer.new(Customer.where("last_name like ?", "%#{params[:last_name]}%"))
-          end
-        end
-      end
-    end
-  end
-end
+# module Api
+#   module V1
+#     module Customers
+#       class SearchController < ApplicationController
+#         def show
+#           if params.keys.include?("id")
+#             render json: CustomerSerializer.new(Customer.find(params[:id]))
+#           elsif params.keys.include?("first_name")
+#             render json: CustomerSerializer.new(Customer.where("first_name like ?", "%#{params[:first_name]}%"))
+#           else
+#             render json: CustomerSerializer.new(Customer.where("last_name like ?", "%#{params[:last_name]}%"))
+#           end
+#         end
+#
+#         def index
+#           if params.keys.include?("first_name")
+#             render json: CustomerSerializer.new(Customer.where("first_name like ?", "%#{params[:first_name]}%"))
+#           else
+#             render json: CustomerSerializer.new(Customer.where("last_name like ?", "%#{params[:last_name]}%"))
+#           end
+#         end
+#       end
+#     end
+#   end
+# end
--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -1,31 +1,31 @@
-module Api
-  module V1
-    class InvoiceItemsController < ApplicationController
-      def index
-        render json: InvoiceItemSerializer.new(InvoiceItem.all)
-      end
-
-      def show
-        render json: InvoiceItemSerializer.new(InvoiceItem.find(params[:id]))
-      end
-
-      def create
-        render json: InvoiceItemSerializer.new(InvoiceItem.create(invoice_item_params))
-      end
-
-      def update
-        render json: InvoiceItemSerializer.new(InvoiceItem.update(params[:id], invoice_item_params))
-      end
-
-      def destroy
-        render json: InvoiceItem.delete(params[:id])
-      end
-
-      private
-
-      def invoice_item_params
-        params.require(:invoice_item).permit(:item_id, :invoice_id, :quantity, :unit_price)
-      end
-    end
-  end
-end
+# module Api
+#   module V1
+#     class InvoiceItemsController < ApplicationController
+#       def index
+#         render json: InvoiceItemSerializer.new(InvoiceItem.all)
+#       end
+#
+#       def show
+#         render json: InvoiceItemSerializer.new(InvoiceItem.find(params[:id]))
+#       end
+#
+#       def create
+#         render json: InvoiceItemSerializer.new(InvoiceItem.create(invoice_item_params))
+#       end
+#
+#       def update
+#         render json: InvoiceItemSerializer.new(InvoiceItem.update(params[:id], invoice_item_params))
+#       end
+#
+#       def destroy
+#         render json: InvoiceItem.delete(params[:id])
+#       end
+#
+#       private
+#
+#       def invoice_item_params
+#         params.permit(:item_id, :invoice_id, :quantity, :unit_price)
+#       end
+#     end
+#   end
+# end
