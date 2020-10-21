5fd66f939dd76cd8c7edf89e3dd82d5eacf75f0b
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-21 15:56:44 -0600

    Add start to most_revenue method to merchants search controller

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -13,6 +13,32 @@ module Api
         def index
           render json: MerchantSerializer.new(Merchant.where("name like ?", "%#{params[:name]}%"))
         end
+
+        def most_revenue
+
+          # SELECT "invoice_items"."item_id", "status", "result", "merchant_id" , SUM(invoice_items.quantity*invoice_items.unit_price) AS revenue
+          # FROM "invoice_items"
+          # INNER JOIN "invoices"
+          # ON "invoices"."id" = "invoice_items"."invoice_id"
+          # INNER JOIN "transactions"
+          # ON "invoice_items"."invoice_id" = "transactions"."invoice_id"
+          # ORDER BY revenue DESC
+          # LIMIT 10;
+          merchant_ids = []
+          invoices = Invoice.select("invoices.merchant_id, status, 'transactions.result', SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+                      .joins(:invoice_items, :transactions)
+                      .where(transactions: {result: "success"})
+                      .where(invoices: {status: "shipped"})
+                      .order("revenue DESC")
+                      .limit(params[:quantity])
+                      # .pluck(:merchant_id)
+
+          invoices.each do |invoice|
+            merchant_ids << invoice[:merchant_id]
+          end
+
+          render json: MerchantSerializer.new(Merchant.where(id: merchant_ids))
+        end
       end
     end
   end
