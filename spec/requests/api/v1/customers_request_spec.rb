5a30d161b09ad32da1dbdc38f31386f686aae7b0
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-20 18:22:13 -0600

    Update customer spec with test for partial first name

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -136,7 +136,7 @@ describe "Customers API" do
     expect(customer[:data][0][:attributes][:last_name]).to eq(Customer.first.last_name)
   end
 
-  it "can find a single record that matches a first name" do
+  it "can find a single record that matches a last name" do
     create_list(:customer, 3)
     get '/api/v1/customers'
 
@@ -157,4 +157,27 @@ describe "Customers API" do
     expect(customer[:data][0][:attributes]).to have_key(:last_name)
     expect(customer[:data][0][:attributes][:last_name]).to eq(Customer.first.last_name)
   end
+
+  it "can find a single record that matches a partial first name" do
+    create_list(:customer, 3)
+    get '/api/v1/customers'
+
+    attribute = "first_name"
+    value = Customer.first.first_name[0..3]
+
+    get "/api/v1/customers/find?#{attribute}=#{value}"
+    expect(response).to be_successful
+
+    customer = JSON.parse(response.body, symbolize_names: true)
+
+    expect(customer[:data][0][:attributes]).to have_key(:id)
+    expect(customer[:data][0][:attributes][:id]).to eq(Customer.first.id)
+
+    expect(customer[:data][0][:attributes]).to have_key(:first_name)
+    expect(customer[:data][0][:attributes][:first_name]).to eq(Customer.first.first_name)
+
+    expect(customer[:data][0][:attributes]).to have_key(:last_name)
+    expect(customer[:data][0][:attributes][:last_name]).to eq(Customer.first.last_name)
+  end
+
 end
