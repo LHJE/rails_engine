2e33663d5ee94756d086ab8a1b61c5069737d6c5
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-20 21:07:43 -0600

    Re-add load method to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,16 @@
+require 'csv'
+
 class Customer < ApplicationRecord
   has_many :invoices, through: :invoice
 
   validates :first_name,
             :last_name, presence: true
+
+  def self.load_customers(file_path)
+    customers = []
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |data|
+      customers << Customer.create!(first_name: data.to_h[:first_name], last_name: data.to_h[:last_name])
+    end
+    customers
+  end
 end
