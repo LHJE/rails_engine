cd6910938aa409e8469d1af43acd8af697b451e9
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-20 19:16:48 -0600

    Update merchant search controller with search logic

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -3,7 +3,11 @@ module Api
     module Merchants
       class SearchController < ApplicationController
         def show
-
+          if params.keys.include?("id")
+            render json: MerchantSerializer.new(Merchant.find(params[:id]))
+          else
+            render json: MerchantSerializer.new(Merchant.where("name like ?", "%#{params[:name]}%"))
+          end
         end
       end
     end
