382a209f77568612736a6689ee184ac41e10dfd1
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-21 17:39:45 -0600

    Update merchant controller delete method to pass rspec

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -18,7 +18,7 @@ module Api
       end
 
       def destroy
-        render json: Merchant.delete(params[:id])
+        Merchant.delete(params[:id])
       end
 
       def items
@@ -28,7 +28,7 @@ module Api
       private
 
       def merchant_params
-        params.require(:merchant).permit(:name)
+        params.permit(:name)
       end
     end
   end
