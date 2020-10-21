b02fcd2346edcd789aecd44952fb90700a6c8edc
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-20 21:29:22 -0600

    Update invoice search controller for find_all, multiple records

--- app/controllers/api/v1/invoices/search_controller.rb
+++ app/controllers/api/v1/invoices/search_controller.rb
@@ -13,6 +13,16 @@ module Api
             render json: InvoiceSerializer.new(Invoice.where("status like ?", "%#{params[:status]}%"))
           end
         end
+
+        def index
+          if params.keys.include?("customer_id")
+            render json: InvoiceSerializer.new(Invoice.where("customer_id like ?", "%#{params[:customer_id]}%"))
+          elsif params.keys.include?("merchant_id")
+            render json: InvoiceSerializer.new(Invoice.where("merchant_id like ?", "%#{params[:merchant_id]}%"))
+          else
+            render json: InvoiceSerializer.new(Invoice.where("status like ?", "%#{params[:status]}%"))
+          end
+        end
       end
     end
   end
