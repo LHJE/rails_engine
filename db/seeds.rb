fd4c4211be84ed6a9def3d15a62b9e2c00ea3ffa
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-22 15:39:45 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-Customer.delete_all
+Customer.destroy_all
 Customer.reset_pk_sequence
-Merchant.delete_all
+Merchant.destroy_all
 Merchant.reset_pk_sequence
-Item.delete_all
+Item.destroy_all
 Item.reset_pk_sequence
-Invoice.delete_all
+Invoice.destroy_all
 Invoice.reset_pk_sequence
-InvoiceItem.delete_all
+InvoiceItem.destroy_all
 InvoiceItem.reset_pk_sequence
-Transaction.delete_all
+Transaction.destroy_all
 Transaction.reset_pk_sequence
 
 # This file should contain all the record creation needed to seed the database with its default values.
